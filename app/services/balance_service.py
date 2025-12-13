93238b53d44f27ece18333418334cc7bbf331ace
svedant1207 <142200051+svedant1207@users.noreply.github.com>
2025-12-13 23:12:48 +0530

    test failing fixed

--- app/services/balance_service.py
+++ app/services/balance_service.py
@@ -5,62 +5,73 @@ from app.models.expense_split import ExpenseSplit
 
 class BalanceService:
     @staticmethod
+    def user_balances():
+        """
+        Returns:
+        {
+            user_id: net_balance
+        }
+
+        +ve  -> user should receive money
+        -ve  -> user owes money
+        """
+
+        balances = defaultdict(float)
+
+        # Payer gets full expense amount
+        for expense in Expense.query.all():
+            balances[expense.paid_by_id] += float(expense.amount)
+
+        # Each split reduces user's balance
+        for split in ExpenseSplit.query.all():
+            balances[split.user_id] -= float(split.amount)
+
+        return {uid: round(amount, 2) for uid, amount in balances.items()}
+
+    @staticmethod
     def user_pairwise_debts():
         """
-        Returns a dict of dicts:
+        Returns pairwise debts:
         {
-            user_id_A: {
-                user_id_B: amount_A_owes_B
+            debtor_id: {
+                creditor_id: amount
             }
         }
-        Only returns positive debts (A owes B).
+
+        Only positive debts are returned.
         """
-        # debts[debtor][creditor] = amount
         debts = defaultdict(lambda: defaultdict(float))
 
-        # We need to look at each expense and its splits
-        expenses = Expense.query.all()
-        
-        for expense in expenses:
+        # Build raw debts from expenses
+        for expense in Expense.query.all():
             payer_id = expense.paid_by_id
-            
+
             for split in expense.splits:
                 if split.user_id == payer_id:
-                    continue  # Paying for yourself creates no debt
-                
-                # split.user_id owes payer_id split.amount
-                amount = float(split.amount)
-                
-                # Add to raw debt
-                debts[split.user_id][payer_id] += amount
+                    continue  # No debt to self
 
-        # Simplify pairwise
-        # If A owes B $10 and B owes A $5 -> A owes B $5
+                debts[split.user_id][payer_id] += float(split.amount)
+
+        # Simplify mutual debts
         final_debts = defaultdict(dict)
-        
-        # Get all unique pairs
+
         users = set(debts.keys())
-        for d in debts.values():
-            users.update(d.keys())
+        for inner in debts.values():
+            users.update(inner.keys())
         users = list(users)
 
-        processed_pairs = set()
-
         for i in range(len(users)):
-            u1 = users[i]
             for j in range(i + 1, len(users)):
-                u2 = users[j]
-                
-                # Check debt u1 -> u2
+                u1, u2 = users[i], users[j]
+
                 d1 = debts[u1][u2]
-                # Check debt u2 -> u1
                 d2 = debts[u2][u1]
-                
+
                 net = d1 - d2
-                
-                if net > 0.009: # u1 owes u2
+
+                if net > 0.009:
                     final_debts[u1][u2] = round(net, 2)
-                elif net < -0.009: # u2 owes u1
+                elif net < -0.009:
                     final_debts[u2][u1] = round(-net, 2)
-                    
+
         return final_debts
