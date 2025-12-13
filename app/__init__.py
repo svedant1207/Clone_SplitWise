bf3ba34157dfee8749ec76b0a1ff3a789bc5931e
svedant1207 <142200051+svedant1207@users.noreply.github.com>
2025-12-13 11:28:47 +0530

    forgot init

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,16 @@
 from flask import Flask
 from app.extensions import db
 from config import Config
-
+from app.models.user import User
+from app.models.expense import Expense
+from app.models.expense_split import ExpenseSplit
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
-
+    
     db.init_app(app)
-
+    
     @app.route("/")
     def health_check():
         return {"status": "ok"}
